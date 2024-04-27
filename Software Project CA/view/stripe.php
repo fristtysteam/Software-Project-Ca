02428217db53b525ee6d2d7c92c9e1daf34ac9ef
Marco <fristtysteam@gmail.com>
2024-04-27 12:35:37 +0100

    added proper validation for payment page

--- Software Project CA/view/stripe.php
+++ Software Project CA/view/stripe.php
@@ -18,6 +18,7 @@ $totalPrice = isset($_GET['totalPrice']) ? $_GET['totalPrice'] : 0;
     <link rel="stylesheet" href="../css/paymentDetailsStripe.css">
     <script src="../js/paymentDetailsStripe.js"></script>
 </head>
+<body>
 <br>
 <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
 
@@ -25,48 +26,46 @@ $totalPrice = isset($_GET['totalPrice']) ? $_GET['totalPrice'] : 0;
     <div id="payment-stripe" class="container">
         <h2>Payment details</h2>
         <p>Total to pay: €<?php echo number_format($totalPrice, 2); ?></p>
-        <div class="flex">
-            <div class="list full">
-                <section class="form-group">
-                    <label for="cc-number" class="control-label">Credit Card Number </label>
-                    <div class="input-group">
-                        <span class="input-group-addon"><i class="fa fa-credit-card"></i></span>
-                        <input id="cc-number" type="tel" class="input-lg form-control cc-number" autocomplete="cc-number" placeholder="4319 0000 1111 1234" data-payment='cc-number' required>
-                    </div>
-                    <section>
+        <form action="../controller/index.php?action=pay" method="post" id="payment-form">
+            <div class="flex">
+                <div class="list full">
+                    <section class="form-group">
+                        <label for="cc-number" class="control-label">Credit Card Number </label>
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="fa fa-credit-card"></i></span>
+                            <input id="cc-number" type="tel" class="input-lg form-control cc-number" autocomplete="cc-number" placeholder="4319 0000 1111 1234" data-payment='cc-number' required pattern="\d{4} \d{4} \d{4} \d{4}">
+                        </div>
+                    </section>
+                </div>
+                <div class="list">
+                    <section class="form-group">
+                        <label>Expiration Date</label>
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
+                            <input id="cc-exp" type="tel" class="input-lg form-control cc-exp" autocomplete="cc-exp" placeholder="MM / YYYY" data-payment='cc-exp' required pattern="\d{2}\/\d{4}">
+                        </div>
+                    </section>
+                </div>
+                <div class="list">
+                    <section class="form-group">
+                        <label>Security Code</label>
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="fa fa-lock"></i></span>
+                            <input id="cc-cvc" type="tel" class="input-lg form-control cc-cvc" autocomplete="off" placeholder="CVC" data-payment='cc-cvc' required pattern="\d{3}">
+                        </div>
+                    </section>
+                </div>
+                <div class="dynamic-content">
+                    Please ensure details you provide are correct.</br>
+                    Your Credit/Debit card will be charged with
+                    extra for purchases outside of Europe.
+                </div>
             </div>
-            <div class="list">
-                <section class="form-group">
-                    <label>Expiration Date</label>
-                    <div class="input-group">
-                        <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
-                        <input id="cc-exp" type="tel" class="input-lg form-control cc-exp" autocomplete="cc-exp" placeholder="MM / YYYY" data-payment='cc-exp' required>
-                    </div>
-                </section>
-            </div>
-            <div class="list">
-                <section class="form-group">
-                    <label>Security Code</label>
-                    <div class="input-group">
-                        <span class="input-group-addon"><i class="fa fa-lock"></i></span>
-                        <input id="cc-cvc" type="tel" class="input-lg form-control cc-cvc" autocomplete="off" placeholder="CVC" data-payment='cc-cvc' required>
-                    </div>
-                </section>
-            </div>
-            <div class="dynamic-content">
-                Please ensure details you provide are correct.</br>
-                Your Credit/Debit card will be charged with
-                extra for purchases outside of Europe.
-            </div>
-        </div>
-        <a href="../controller/index.php?action=pay"   name="pay" class="btn btn-primary">Pay</a>
-
+            <button type="submit" name="pay" class="btn btn-primary">Pay</button>
+        </form>
     </div>
-
-
 </section>
 <br/>
-</br>
 </body>
 </html>
 <?php include 'footer.php'; ?>
